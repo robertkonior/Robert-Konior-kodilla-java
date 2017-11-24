65c21e9775ed8a4655efc4bec3f2a4412acd75d6
Robert Konior <praca.robert.konior@gmail.com>
2017-11-24 17:15:17 +0000

    10.10

--- kodilla-spring/src/test/java/com/kodilla/spring/SpringRunnerTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/SpringRunnerTestSuite.java
@@ -7,7 +7,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -37,4 +37,6 @@ public class SpringRunnerTestSuite {
 		//Then
 		Assert.assertEquals("This is a triangle.", name);
 	}
+
+
 }
