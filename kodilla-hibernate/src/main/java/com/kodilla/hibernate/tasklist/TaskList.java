bf781b2e771ba6d3527585aea50eee2bf2c06284
robertkonior <praca.robert.konior@gmail.com>
2017-12-08 18:15:14 +0000

    13.2/0.1

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -0,0 +1,58 @@
+package com.kodilla.hibernate.tasklist;
+
+import com.kodilla.hibernate.task.Task;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name="TASKLISTS")
+public final class TaskList {
+    private int id;
+    private String listName;
+    private String description;
+    private List<Task> tasks = new ArrayList<>();
+
+    public TaskList(){}
+
+    public TaskList(String listName, String description) {
+        this.listName = listName;
+        this.description = description;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+    @NotNull
+    @Column(name = "LISTNAME")
+    public String getListName() {
+        return listName;
+    }
+
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+}
