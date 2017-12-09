48eac04fe560c0f2c6ff07117e3d20cdb8ed80ae
robertkonior <praca.robert.konior@gmail.com>
2017-12-09 02:07:46 +0000

    13.3/0.3

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/dao/TaskListDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/dao/TaskListDao.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Transactional
 @Repository
-public interface TaskListDao extends CrudRepository<TaskList,String> {
+public interface TaskListDao extends CrudRepository<TaskList,Integer> {
     List<TaskList> findByListName(String listName);
 }
