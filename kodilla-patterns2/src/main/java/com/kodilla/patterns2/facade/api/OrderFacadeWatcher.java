2086dc7386d510f23d88bfc5d35d4e9390fad9c5
robertkonior <praca.robert.konior@gmail.com>
2018-02-14 21:51:31 +0100

    20.3/1

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacadeWatcher.java
@@ -0,0 +1,39 @@
+package com.kodilla.patterns2.facade.api;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Aspect
+@Component
+public class OrderFacadeWatcher {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderFacadeWatcher.class);
+
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public void logEvent() {
+        LOGGER.info("Starting the Ordering Process..  ");
+    }
+
+    @Around("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public Object measureTime(final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result;
+        try {
+            long begin = System.currentTimeMillis();
+            result = proceedingJoinPoint.proceed();
+            long end = System.currentTimeMillis();
+            LOGGER.info("Ordering process consumed " + (end-begin) + "[ms]");
+        } catch (Throwable throwable) {
+            LOGGER.error(throwable.getMessage());
+            throw  throwable;
+        }
+        return result;
+    }
+
+}
