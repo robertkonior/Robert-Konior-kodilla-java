b9facd7280c49adebf4121cdb48136078916e443
robertkonior <praca.robert.konior@gmail.com>
2018-03-01 19:22:10 +0100

    23.3/2

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -8,19 +8,15 @@ import org.openqa.selenium.support.ui.Select;
 
 public class FacebookTestingApp {
 
-    public static final String XPATH_SELECT = "//div[@class=\\\"_5k_5\\\"]/span/span";
+    public static final String XPATH_SELECT = "//div[@class=\"_5k_5\"]/span/span";
     public static final String XPATH_DAY = XPATH_SELECT +"/select[1]";
     public static final String XPATH_MONTH = XPATH_SELECT  +"/select[2]";
     public static final String XPATH_YEAR = XPATH_SELECT  +"/select[3]";
-    public static final String XPATH_WAIT_FOR = "//select[1]";
-
 
     public static void main(String[] args) {
 
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
         driver.get("https://www.facebook.com/");
-
-        while(!driver.findElement(By.xpath(XPATH_WAIT_FOR)).isDisplayed());
 
         WebElement selectComboDay = driver.findElement(By.xpath(XPATH_DAY));
         Select selectDay = new Select(selectComboDay);
@@ -32,6 +28,6 @@ public class FacebookTestingApp {
 
         WebElement selectComboYear = driver.findElement(By.xpath(XPATH_YEAR));
         Select selectYear = new Select(selectComboYear);
-        selectYear.selectByIndex(22);
+        selectYear.selectByValue("1990");
     }
 }
